1d52b6c40be9b5398b1349c2b7cdee37548f5f4a
chenqiwei123 <1049563035@qq.com>
2022-04-20 21:21:56 +0800

    Initial commit

---
+++ src/main/java/com/example/getvideoinfo/GetVideoInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.getvideoinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetVideoInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetVideoInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/getvideoinfo/GetVideoInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getvideoinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetVideoInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
